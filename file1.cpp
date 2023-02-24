fac7c209203a2a3c9293c176deb4270ef15e2920
xyz <xyz@infinera.com>
2023-02-24 19:30:54 +0530

    some bugs are fixed

--- file1.cpp
+++ file1.cpp
@@ -1,11 +1,9 @@
 This is newMoon folder
 new line is added to the file1
-<<<<<<< Updated upstream
 
 
 after merging the file is reeditted for too many changes
-1
-=======
+
 changes are done in dummy branch
-dfdf
->>>>>>> Stashed changes
+
+changes are done in dummy branch
