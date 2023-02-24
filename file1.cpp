7b349e37660699d529e7721ce26ca429ed513a00
xyz <xyz@infinera.com>
2023-02-24 11:34:46 +0530

    file1 is modified in master

--- file1.cpp
+++ file1.cpp
@@ -1,12 +1,5 @@
 This is newMoon folder
 new line is added to the file1
 
-<<<<<<< HEAD
-new line added 
-hey
-hey hey
-=======
-in new features branch data is changed
-again changed for hard reset
->>>>>>> new_features
- New line added to the file1 to test forward merge 
+
+after merging the file is reeditted for too many changes
