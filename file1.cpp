c28e9bce6ee880cf7659484118f546e37e80238d
xyz <xyz@infinera.com>
2023-02-24 11:36:10 +0530

    Changes are done in dummy branch

--- file1.cpp
+++ file1.cpp
@@ -1,11 +1,3 @@
 This is newMoon folder
 new line is added to the file1
-
-<<<<<<< HEAD
-new line added 
-hey
-hey hey
-=======
-in new features branch data is changed
-again changed for hard reset
->>>>>>> new_features
+changes are done in dummy branch
