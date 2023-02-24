3972367fe9ed140c0c305a8bd5c426b12ddbe154
xyz <xyz@infinera.com>
2023-02-24 14:16:12 +0530

    commit

--- file1.cpp
+++ file1.cpp
@@ -1,6 +1,11 @@
 This is newMoon folder
 new line is added to the file1
+<<<<<<< Updated upstream
 
 
 after merging the file is reeditted for too many changes
 1
+=======
+changes are done in dummy branch
+dfdf
+>>>>>>> Stashed changes
